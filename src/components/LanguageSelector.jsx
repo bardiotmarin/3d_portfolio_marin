2c39b588e4e59a37a49da478df5b4d90036fcab5
BleuCube <59800062+bardiotmarin@users.noreply.github.com>
2023-07-26 17:34:05 +0200

    push the project

---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,25 @@
+// LanguageSelector.jsx
+import React from "react";
+import { useTranslation } from "react-i18next";
+
+const LanguageSelector = () => {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (event) => {
+    const selectedLanguage = event.target.value;
+    i18n.changeLanguage(selectedLanguage);
+  };
+
+  return (
+    <div>
+      <label htmlFor="language-select">Select Language:</label>
+      <select id="language-select" onChange={changeLanguage}>
+        <option value="en">English</option>
+        <option value="fr">Français</option>
+        {/* Add more language options if needed */}
+      </select>
+    </div>
+  );
+};
+
+export default LanguageSelector;
