d83ba4bfbf0e3cdfbd3ff72d940aad2c18f62882
WeDrumsFr <109983967+WeDrumsFr@users.noreply.github.com>
2025-11-22 16:30:29 +0100

    test new ui

--- src/components/canvas/index.js
+++
@@ -1,8 +0,0 @@
-import EarthCanvas from "./Earth";
-import BallCanvas from "./Ball";
-import MewCanvas from "./Mew";
-import StarsCanvas from "./Stars";
-import Lotus from "./Lotus.jsx";
-import MoonHero from "./Moon.jsx";
-
-export { EarthCanvas, BallCanvas, MewCanvas, StarsCanvas, Lotus, MoonHero };
