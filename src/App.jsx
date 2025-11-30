f6606cb7e8adec23211bb87cff61fb4c9dcc89fd
WeDrumsFr <109983967+WeDrumsFr@users.noreply.github.com>
2025-11-30 20:59:16 +0100

    no buterfly on prod

--- src/App.jsx
+++ src/App.jsx
@@ -59,39 +59,6 @@ const App = () => {
           transition: 'opacity 0.5s ease-out',
           pointerEvents: isLoading ? 'all' : 'none'
         }}>
-          <Canvas>
-            <PreloadAssets />
-            <LoaderMain onLoadComplete={handleLoadComplete} />
-            <Preload all />
-          </Canvas>
-        </div>
-
-
-        {/* 🎨 CONTENU PRINCIPAL */}
-        <div style={{
-          opacity: !isLoading ? 1 : 0,
-          transition: 'opacity 0.5s ease-in',
-          pointerEvents: !isLoading ? 'all' : 'none'
-        }}>
-          {/* 🦋 Canvas papillon - Caché sur mobile (< md:768px), responsive sur desktop */}
-          <Canvas
-            id="papillon-canvas"
-            className="hidden md:block fixed top-0 left-0 w-full h-full pointer-events-none scale-75 md:scale-90 lg:scale-100"
-            shadows
-            dpr={[1, 2]}
-            gl={{ preserveDrawingBuffer: true }}
-            camera={{ fov: 45, near: 0.1, far: 200, position: [0, 0, 6] }}
-            style={{
-              position: "fixed",
-              top: 0,
-              left: 0,
-              width: "100vw",
-              height: "100vh",
-              pointerEvents: "none"
-            }}
-          >
-            <Papillon />
-          </Canvas>
 
 
           {/* Contenu de la page */}
